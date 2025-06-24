95fa2d81eda49939ef3b64d62c114aa553c2448a
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-06-24 11:24:19 +0200

    mis à jour de la structure du quiz

--- game.js
+++ game.js
@@ -1,19 +1,30 @@
-import {quiz_frida_kahlo} from './questions.js';
+import { quiz_frida_kahlo } from './questions.js'; //  nous avons relié ce fichier avec questions.js
 
+// selection des elements HTML
 const quizContainer = document.querySelector("#quizContainer");//on fait correspondre la variable quizContainer à son emplacement HTML
 const questionText = document.querySelector("#questionText"); //on fait correspondre la variable questionText à son emplacement HTML
 const options = document.querySelector(".options"); //on fait correspondre la variable questions à son emplacement HTML
+const nextButton = document.querySelector("#nextButton");
 
+//parametres de base de nos fonctions
 let currentQuestionIndex = 0; // permet d'afficher la question et les boutons 0 au round 0
-
+const currentQuestionPage = quiz_frida_kahlo.questions[currentQuestionIndex];
 //const firstQuestion = quiz_frida_kahlo.questions[0].text;
 
-function loadQuestion () { // Fonction pour afficher une question basée sur l'index actuel
-    options.innerHTML = '';// Vider le conteneur des options
-    questionText.innerHTML = `<h2>${quiz_frida_kahlo.questions[currentQuestionIndex].text}</h2>`; // on fait apparaitre l'intitulé de question, variant à chaque currentQuestionIndex
-    for(const item of quiz_frida_kahlo.questions[currentQuestionIndex].options) {
-    options.innerHTML += `<button>${item}</button>`;
-}; // la boucle fait apparaitre 4 éléments, à chaque currentQuestionIndex
-};
+//déclaration des fonctions
 
-loadQuestion(); //on appelle la fonction pour l'executer
+
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex += 1;
+});
+
+// function loadQuestion() { // Fonction pour afficher une question basée sur l'index actuel
+//     options.innerHTML = '';// Vider le conteneur des options
+//     questionText.innerHTML = `<h2>${currentQuestionPage.text}</h2>`; // on fait apparaitre l'intitulé de question, variant à chaque currentQuestionIndex
+//     for (const item of currentQuestionPage.options) {
+//         options.innerHTML += `<button>${item}</button>`;
+//     }; // la boucle fait apparaitre 4 éléments, à chaque currentQuestionIndex
+// };
+
+// loadQuestion(); //on appelle la fonction pour l'executer
+console.log(currentQuestionIndex);
